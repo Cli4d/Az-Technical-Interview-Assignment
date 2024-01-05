b4f777b29cb09b0f892ebf873e35fc62fdf5b55a
Clifford Ouma <ochiengclifford2000@gmail.com>
2024-01-06 02:55:21 +0300

    Add components to root app

---
+++ azgard-interview/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+//importing components
+// import SignUp from './components/Signup';
+import SignIn from './components/Signin';
+import Illustration from './components/Illustration';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Illustration />
+          </div>
+          <div className="col">
+            <SignIn />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
