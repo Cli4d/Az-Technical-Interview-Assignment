d3cb4e84dbebe88b2d9007de3be8a0692d06966b
Clifford Ouma <ochiengclifford2000@gmail.com>
2024-01-06 08:58:30 +0300

    Remove commented code

--- azgard-interview/src/components/Signup.js
+++ azgard-interview/src/components/Signup.js
@@ -46,42 +46,6 @@ function SignUpForm() {
 
       </form>
     </div>
-
-    // <div className = "container">
-    //         <div className = "row justify-content-center">
-    //             <form className = "col-md-4 mt-3 pt-3 pb-3">
-    //                 { "" !== notice &&
-    //                     <div className = "alert alert-warning" role = "alert">
-    //                         { notice }    
-    //                     </div>
-    //                 }
-    //                 <div className = "form-floating mb-3">
-    //                     <input id = "signupEmail" type = "email" className = "form-control" aria-describedby = "emailHelp" placeholder = "name@example.com" value = { email } onChange = { (e) => setEmail(e.target.value) }></input>
-    //                     <label htmlFor = "signupEmail" className = "form-label">Enter an email address for your username</label>
-    //                 </div>
-    //                 <div className = "form-floating mb-3">
-    //                     <input id = "signupPassword" type = "password" className = "form-control" placeholder = "Password" value = { password } onChange = { (e) => setPassword(e.target.value) }></input>
-    //                     <label htmlFor = "signupPassword" className = "form-label">Password</label>
-    //                 </div>
-    //                 <div className = "form-floating mb-3">
-    //                     <input id = "confirmPassword" type = "password" className = "form-control" placeholder = "Confirm Password" value = { confirmPassword } onChange = { (e) => setConfirmPassword(e.target.value) }></input>
-    //                     <label htmlFor = "confirmPassword" className = "form-label">Confirm Password</label>
-    //                 </div>                    
-    //                 <div className = "d-grid">
-    //                     <button type = "submit" className = "btn btn-primary pt-3 pb-3" onClick = {(e) => handleSubmition(e)}>Signup</button>
-    //                 </div>
-    //                 <div className = "mt-3 text-center">
-    //                     <span>Go back to login?</span>
-    //                 </div>                    
-    //             </form>
-    //         </div>
-    //     </div>
-    // <form onSubmit={handleSubmition}>
-    //   <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-    //   <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
-    //   <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-    //   <button type="submit">Sign Up</button>
-    // </form>
   );
 }
 
