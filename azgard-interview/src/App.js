a532413e1e85cfa2daf478ffda14ab775e2a9796
Clifford Ouma <ochiengclifford2000@gmail.com>
2024-01-07 12:52:59 +0300

    Restyle root app page

--- azgard-interview/src/App.js
+++ azgard-interview/src/App.js
@@ -4,16 +4,12 @@ import './App.css';
 //importing components
 // import SignUp from './components/Signup';
 import SignIn from './components/Signin';
-import Illustration from './components/Illustration';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
+    <div className="App bg-light">
+      <div className="container-fluid">
         <div className="row">
-          {/* <div className="col-12 col-md-6 d-none d-md-inline">
-            <Illustration />
-          </div> */}
           <div className="col-12">
             <SignIn />
           </div>
